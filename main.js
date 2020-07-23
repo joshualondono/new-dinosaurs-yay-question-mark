2684d16d895844e21744cb6ace7f0dbcf3d52338
joshua londono <joshualondono@outlook.com>
2020-07-23 11:45:33 -0400

    cleaned up all the extra returns, mutate error for function makeSingular

--- main.js
+++ main.js
@@ -5,40 +5,31 @@ const makeDino = function(name, period, diet, extinct) {
       carnivore: diet,
       extinct: extinct
     }
-
     if(obj.extinct === undefined){
       obj.extinct = false
-      return obj
-
     } else{
       obj.extinct === extinct
-    }
-    return obj
+    } return obj
   }
 
 const makeSingular = function(obj) {
   if(obj.species.lastIndexOf('us') === -1){
-    return obj
   } else {
-    let indexStart = obj.species.lastIndexOf('us');
-    obj.species = obj.species.slice(0, indexStart)
-    return obj
-  }
+    const sliceEnd = obj.species.lastIndexOf('us');
+    obj.species = obj.species.slice(0, sliceEnd)
+  } return obj
 }
 
 const truncateSpecies = function(obj) {
   if(obj.species.length > 10){
     obj.species = obj.species.slice(0, 7) + '...'
-    return obj
   } return obj
 }
 
 const makeExtinct = function(obj) {
    if(obj.extinct === false){
      obj.extinct = true
-     return obj
    } return obj
-
 }
 
 module.exports = {
